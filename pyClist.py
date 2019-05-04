8009dddcccd395ea282e339d7e5cbdcbcd5d987f
Jitender Singh <jitushekhawat1994@gmail.com>
2019-05-04 10:35:02 +0530

    python to cpp list tuple passed

--- pyClist.py
+++ pyClist.py
@@ -1,7 +1,8 @@
 def mysolve(vals):
     print vals
     print "Len is :" , len(vals)
-    print "First element is:" , vals[0]
-    print "second element is:" , vals[1]
-    print "third element is:" , vals[2]
-    return 0
+    mylist = []
+    for i in range(10):
+        mylist.append(vals[i])
+    print "mylist is : " , mylist
+    return mylist
